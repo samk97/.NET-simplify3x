03b59dcc5325ebee64c203381b89232fc324d624
am12it <amit.2021ca017@mnnit.ac.in>
2024-01-22 16:35:40 +0530

    testing

---
+++ Amit/test.cs
@@ -0,0 +1,102 @@
+public class BanckAccount
+
+{
+
+
+
+    //readonly 
+
+    public string AccontHolder;
+
+    public double Balance;
+
+
+
+    //constuructor for the initilizing 
+
+    public BanckAccount(string account, double balance)
+
+    {
+
+        AccontHolder = account;
+
+        Balance = balance;
+
+
+
+    }
+
+    public void deposit(double amount)
+
+    {
+
+        if (amount > 0)
+
+        {
+
+            Balance += amount;
+
+            Console.WriteLine($"Deposit of {amount} successful and newBalance is {Balance}");
+
+        }
+
+        else
+
+        {
+
+            Console.WriteLine("invalid deposit amount");
+
+        }
+
+    }
+
+    public void withDrwa(double amount)
+
+    {
+
+        if (amount > 0 && amount <= Balance)
+        {
+
+            Balance -= amount;
+
+            Console.WriteLine($"Amout is successfuly deposited{amount} and left amount is the {Balance}");
+
+        }
+
+        else
+
+        {
+
+            Console.WriteLine("Invalid amount");
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            BanckAccount myAccount = new BanckAccount("Amit Kumar", 20000);
+
+            Console.WriteLine($"Account holder name {myAccount.AccontHolder}");
+
+            Console.WriteLine($"Balance is {myAccount.Balance}");
+
+            myAccount.deposit(400);
+
+            myAccount.withDrwa(300);
+
+
+
+        }
+
+    }
+
+
+
+}
